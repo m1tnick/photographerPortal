4f91a07b1d9e21f5558e2db06bb7fc6cc54282e8
Pedro Fernandes <pedroice@gmail.com>
2019-07-06 12:13:44 +0200

    closes #2 add env variables

--- api/server.js
+++ api/server.js
@@ -1,24 +1,30 @@
-// server.js
-
 const express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     mongoose = require('mongoose'),
-    config = require('./DB');
+    chalk = require("chalk"),
+    Config = require(path.resolve("./configuration"));
 
+const config = Config.getConfig();
+const dbUrl = 'mongodb://' + config.HOST + ':27017/' + config.MONGODB;
+
+// MONGOOSE DB
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+mongoose.connect(dbUrl, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)}
 );
 
-
+// EXPRESS
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-const port = process.env.PORT || 4000;
 
-const server = app.listen(function(){
-    console.log('Listening on port ' + port);
+const server = app.listen(config.PORT, () => {
+  console.log(
+    chalk`{green Node Js server running on {green.bold ${
+      config.PORT
+      }} port at {green.bold ${config.MODE_TYPE}}..}`
+  );
 });
