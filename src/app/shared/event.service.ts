02cef4e0fc5d35d69e27f04348ff135fe204c1b3
Pedro Fernandes <pedroice@gmail.com>
2019-08-30 23:37:44 +0200

    chore: bump versions

--- src/app/shared/event.service.ts
+++ src/app/shared/event.service.ts
@@ -2,53 +2,51 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EventService {
-  uri = 'http://localhost:3005/events';
+    uri = 'http://localhost:3005/events';
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  create(name, date, type) {
-    const obj = {
-      name: name,
-      date: date,
-      type: type
-    };
-    console.log(obj);
-    this.http.post(`${this.uri}/add`, obj)
-        .subscribe(res => console.log('Done'));
-  }
+    create(name, date, type) {
+        const obj = {
+            name: name,
+            date: date,
+            type: type
+        };
+        console.log(obj);
+        this.http.post(`${this.uri}`, obj)
+            .subscribe(res => console.log('Done'));
+    }
 
-  getEvents() {
-    return this
-           .http
-           .get(`${this.uri}`);
-  }
-
-  editEvent(id) {
-    return this
+    getEvents() {
+        return this
             .http
-            .get(`${this.uri}/edit/${id}`);
-  }
+            .get(`${this.uri}`);
+    }
 
-  updateEvent(name, date, type, id) {
-    const obj = {
-      name: name,
-      date: date,
-      type: type
-    };
-  this
-    .http
-    .post(`${this.uri}/update/${id}`, obj)
-    .subscribe(res => console.log('Done'));
-  }
+    editEvent(id) {
+        return this
+            .http
+            .get(`${this.uri}/${id}`);
+    }
 
-  deleteEvent(id) {
-    return this
-              .http
-              .get(`${this.uri}/delete/${id}`);
-  }
+    updateEvent(name, date, type, id) {
+        const obj = {
+            name: name,
+            date: date,
+            type: type
+        };
+
+        this.http
+            .patch(`${this.uri}/${id}`, obj)
+            .subscribe(res => console.log('Done'));
+    }
+
+    deleteEvent(id) {
+        return this.http.delete(`${this.uri}/${id}`);
+    }
 }
 
 
