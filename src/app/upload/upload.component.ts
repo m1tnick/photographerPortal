3cf1c6a703d5e36393fde37855da1dd0bd6d9a66
Pedro Fernandes <pedroice@gmail.com>
2019-09-24 21:18:47 +0200

    feat: upload component, #13

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,92 +1,47 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 
-import { UploadService } from './upload.service';
-import { forkJoin, Observable } from 'rxjs';
+import { UploadService, FileQueueObject } from './upload.service';
 
 /*
     Ideias for this component:
         - Click open fullscreen 'window'
         - Drag n Drop area
-        - Progress bar
 */
 
 @Component({
     selector: 'app-upload',
     templateUrl: './upload.component.html'
 })
-export class UploadComponent {
+export class UploadComponent implements OnInit {
     @ViewChild('file') file;
     @Input() eventId: string;
 
     public files: Set<File> = new Set();
-    progress: {
-        [key: string]: {
-            progress: Observable<number>;
-        };
-    };
+    progress: Observable<any>;
+    queue: Observable<FileQueueObject[]>;
 
     uploading = false;
     uploadSuccessful = false;
 
     constructor(public uploadService: UploadService) {}
 
+    ngOnInit() {
+        this.queue = this.uploadService.queue;
+    }
+
     addFiles() {
         this.file.nativeElement.click();
     }
 
-    onFilesAdded() {
-        const files: { [key: string]: File } = this.file.nativeElement.files;
-        for (const key in files) {
-            if (!isNaN(parseInt(key, 10))) {
-                this.files.add(files[key]);
-            }
-        }
+    addToQueue() {
+        const fileBrowser = this.file.nativeElement;
+        this.uploadService.addToQueue(fileBrowser.files);
     }
 
 // https://www.truecodex.com/course/angular-6/file-upload-in-angular-6-7-with-progress-bar-using-web-api
 // https://www.techiediaries.com/angular-file-upload-progress-bar/
     upload() {
-
-        this.files.forEach(file => {
-            // create a new multipart-form for every file
-            const formData: FormData = new FormData();
-            formData.append('file', file, file.name);
-        });
-
-        // if everything was uploaded already, just close the dialog
-        if (this.uploadSuccessful) {
-            return;
-        }
-
-        // set the component state to "uploading"
-        this.uploading = true;
-
-        // start the upload and save the progress map
-        this.progress = this.uploadService.upload(this.eventId, this.files);
-
-        // convert the progress map into an array
-        const allProgressObservables = [];
-        for (const key in this.progress) {
-            if (this.progress[key]) {
-                allProgressObservables.push(this.progress[key].progress);
-            }
-        }
-
-        // Adjust the state variables
-
-        // When all progress-observables are completed...
-        forkJoin(allProgressObservables).subscribe(end => {
-            // ... the dialog can be closed again...
-            // this.canBeClosed = true;
-            // ... the upload was successful...
-            this.uploadSuccessful = true;
-console.log('end');
-console.log(this.progress);
-
-            // ... and the component is no longer uploading
-            this.uploading = false;
-
-            this.files.clear();
-        });
+        this.uploadService.uploadAll(this.eventId);
     }
 }
